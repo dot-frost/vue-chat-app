5135d5d3e1f7b7b3a826161817b30a247934ae1d
Ali Talebi <talebia1993@gmail.com>
2022-01-11 20:45:26 +0330

    add Status Component

---
+++ src/components/User/StatusComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <span class="rounded-full outline outline-offset-2 outline-3 w-full h-full absolute"
+        v-if="type === 'circle'"
+        :class="classList"
+  ></span>
+  <span v-else class="absolute bg-red-600 p-1 rounded-full border border-slate-700" :class="classList"></span>
+</template>
+
+<script>
+export default {
+  name: 'StatusComponent',
+  props: {
+    type: {
+      required: true,
+      type: String
+    },
+    color: {
+      required: false,
+      default: 'green',
+      type: String
+    }
+  },
+  computed: {
+    classList () {
+      const classes = {
+        circle: {
+          green: 'outline-green-500',
+          blue: 'outline-blue-500',
+          red: 'outline-red-500',
+          yellow: 'outline-yellow-500',
+          purple: 'outline-purple-500',
+          gray: 'outline-gray-500',
+          black: 'outline-black'
+        },
+        dot: {
+          green: 'bg-green-500',
+          blue: 'bg-blue-500',
+          red: 'bg-red-500',
+          yellow: 'bg-yellow-500',
+          purple: 'bg-purple-500',
+          gray: 'bg-gray-500',
+          black: 'bg-black'
+        }
+      }
+      return [
+        classes[this.type][this.color]
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
