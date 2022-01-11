1968fdc840d2fa52b91461e39ae00e4ea89434b3
Ali Talebi <talebia1993@gmail.com>
2022-01-11 20:47:54 +0330

    add User Contact Components

---
+++ src/components/User/UserContactComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex justify-start px-5 py-3 gap-2 items-center cursor-pointer">
+    <AvatarComponent :image="user.avatar"
+                     :alt="user.full_name"
+                     :status="user.status"
+                     :status-type="statusType"
+                     class="w-12"/>
+    <div class="hidden md:block">
+      <h3 class="">{{ user.full_name }}</h3>
+      <p class="text-sm" v-if="latest">{{ latest }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import AvatarComponent from '@/components/User/AvatarComponent'
+export default {
+  name: 'UserContactComponent',
+  components: { AvatarComponent },
+  props: {
+    user: {
+      required: true
+    },
+    latest: {
+      default: false
+    },
+    statusType: {
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
