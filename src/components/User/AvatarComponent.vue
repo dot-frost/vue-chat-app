f758c60f2c0afbedc25a3d64b528e79450370fd0
Ali Talebi <talebia1993@gmail.com>
2022-01-11 20:46:02 +0330

    add Avatar Component

---
+++ src/components/User/AvatarComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="rounded-full relative">
+    <StatusComponent :type="statusType" :color="statusColor"/>
+    <img :src="image" :alt="alt" class="w-full rounded-full">
+  </div>
+</template>
+
+<script>
+import StatusComponent from '@/components/User/StatusComponent'
+
+export default {
+  name: 'AvatarComponent',
+  components: { StatusComponent },
+  props: {
+    image: {
+      required: true
+    },
+    alt: {
+      required: true
+    },
+    status: {
+      required: true
+    },
+    statusType: {
+      default: 'dot'
+    }
+  },
+  computed: {
+    statusColor () {
+      return {
+        online: 'green',
+        offline: 'gray'
+      }[this.status]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
