2cb38e23551ecc2603c0d23dd6651ef8db533723
Ali Talebi <talebia1993@gmail.com>
2022-01-11 20:51:30 +0330

    Init Layout for Home View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Hello World!!</h1>
+  <div class="bg-gray-200 h-screen max-h-screen min-h-screen flex justify-center items-center px-10 py-20">
+    <div class="max-w-screen-lg flex-grow h-full overflow-hidden flex flex-coll justify-items-stretch items-stretch">
+      <!-- Sidebar -->
+      <div id="sidebar" class="bg-slate-700 flex flex-col justify-items-stretch text-white w-20 md:w-[300px]">
+      </div>
+      <!-- ChatBox -->
+      <div class="flex-grow bg-white">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
