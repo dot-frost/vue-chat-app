eaf325410166c7007c0abd26e5de249ec69add0a
Ali Talebi <talebia1993@gmail.com>
2022-01-11 20:52:19 +0330

    add sidebar sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,10 @@
     <div class="max-w-screen-lg flex-grow h-full overflow-hidden flex flex-coll justify-items-stretch items-stretch">
       <!-- Sidebar -->
       <div id="sidebar" class="bg-slate-700 flex flex-col justify-items-stretch text-white w-20 md:w-[300px]">
+        <!-- Profile -->
+        <!-- Search -->
+        <!-- Contacts -->
+        <!-- Actions -->
       </div>
       <!-- ChatBox -->
       <div class="flex-grow bg-white">
